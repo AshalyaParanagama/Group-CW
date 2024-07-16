983b0675ac71d8f1f99a846f30db855986eab997
Ashalya Paranagama <ashalya.20230187@iit.ac.lk>
2024-07-16 16:12:40 +0530

    Alignment issue sorted

--- Shop/Shop.js
+++ Shop/Shop.js
@@ -65,7 +65,7 @@ function clearBasket() {
             alert('Please fill out your name and email to proceed with the checkout.');
         } else {
             // Proceed to payment method page
-            window.location.href = "Shop/paymentMethod.html";    
+            window.location.href = "Shop/paymentMethod.html";  
         } 
     });
 
